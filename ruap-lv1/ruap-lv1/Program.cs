7bb1c751b6c644b26868397ddf4fab47f4f9f5c1
MARKO EHMAN <marko.ehman@gmail.com>
2024-11-08 15:14:39 +0100

    cherry-pick main

--- ruap-lv1/ruap-lv1/Program.cs
+++ ruap-lv1/ruap-lv1/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Pozdrav iz aplikacije od Marko Ehman, s indeksom 123456789");
+        Console.WriteLine("Pozdrav iz aplikacije od Marko Ehman");
     }
 }
