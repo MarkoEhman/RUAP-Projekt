8e24e11f0ebdbc8f08ffe4a3391235ba05cb9ac5
MARKO EHMAN <marko.ehman@gmail.com>
2024-11-08 15:16:44 +0100

    adding index to the message

--- ruap-lv1/ruap-lv1/Program.cs
+++ ruap-lv1/ruap-lv1/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Pozdrav iz aplikacije od Marko Ehman");
+        Console.WriteLine("Pozdrav iz aplikacije od Marko Ehman, s indeksom 123456789");
     }
 }
